a91f6d1364c18f6c92fa99433245b260d79e0e93
Aman Soni <amansoni@Amans-MacBook-Pro-2.local>
2025-05-14 20:53:10 +0530

    convert the normal images to avatar

--- app/api/avatars/route.js
+++ app/api/avatars/route.js
@@ -1,34 +1,42 @@
 export async function GET() {
-    const dummyUsers = [
-      {
-        id: 1,
-        firstName: 'Aman',
-        lastName: 'Singh',
-        image: 'https://randomuser.me/api/portraits/men/32.jpg',
-      },
-      {
-        id: 2,
-        firstName: 'Riya',
-        lastName: 'Sharma',
-        image: 'https://randomuser.me/api/portraits/women/44.jpg',
-      },
-      {
-        id: 3,
-        firstName: 'Dev',
-        lastName: 'Patel',
-        image: 'https://randomuser.me/api/portraits/men/75.jpg',
-      },
-    ];
-  
-    return Response.json({ users: dummyUsers });
-  }
-  
+  const dummyUsers = [
+    {
+      id: 1,
+      firstName: 'Salman',
+      lastName: 'Singh',
+      image: 'https://api.dicebear.com/8.x/adventurer/svg?seed=Aman',
+    },
+    {
+      id: 2,
+      firstName: 'Jiya',
+      lastName: 'Sharma',
+      image: 'https://api.dicebear.com/8.x/adventurer/svg?seed=Riya',
+    },
+    {
+      id: 3,
+      firstName: 'Dev',
+      lastName: 'Patel',
+      image: 'https://api.dicebear.com/8.x/adventurer/svg?seed=Dev',
+    },
+    {
+      id: 4,
+      firstName: 'Priya',
+      lastName: 'Mehta',
+      image: 'https://api.dicebear.com/8.x/adventurer/svg?seed=Priya',
+    },
+    {
+      id: 5,
+      firstName: 'Rohan',
+      lastName: 'Verma',
+      image: 'https://api.dicebear.com/8.x/adventurer/svg?seed=Rohan',
+    },
+    {
+      id: 6,
+      firstName: 'Sneha',
+      lastName: 'Kapoor',
+      image: 'https://api.dicebear.com/8.x/adventurer/svg?seed=Sneha',
+    },
+  ];
 
-
-
-
-
-
-
-
-  
+  return Response.json({ users: dummyUsers });
+}
